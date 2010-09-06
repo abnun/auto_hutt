10a6038bbe3068fd04847686ac24abd5b6e96643
root <root@localhost.localdomain>
2010-09-06 12:36:17 +0200

    Neue Logging Einstellungen

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -57,35 +57,44 @@ photo_album.default.thumbnail.height=59
 photo_album.default.big.width=400
 
 // log4j configuration
-log4j =
-{
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    //
+    //appenders {
+    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    //}
+
 	appenders
 	{
-		console name:'stdout', layout:pattern(conversionPattern: '%d{dd.MM.yyyy, HH:mm:ss} [%p] %c{2} %x - %m%n')
+		appender(console(name: 'stdout',
+				layout: pattern(conversionPattern: '[%r] [%d{HH:mm:ss}] [%p] %c{2} %m%n')))
+
+		'null' name: 'stacktrace'
 	}
 
 	root
 	{
-		stdout
-		additivity = true
+		info 'stdout'
+		additivity = false
 	}
 
-	error 'org.codehaus.groovy.grails.web.servlet', //  controllers
-			'org.codehaus.groovy.grails.web.pages', //  GSP
-			'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-			'org.codehaus.groovy.grails."web.mapping.filter', // URL mapping
-			'org.codehaus.groovy.grails."web.mapping', // URL mapping
-			'org.codehaus.groovy.grails.commons', // core / classloading
-			'org.codehaus.groovy.grails.plugins', // plugins
-			'org.codehaus.groovy.grails.orm.hibernate' // hibernate integration
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+	       'org.codehaus.groovy.grails.web.pages', //  GSP
+	       'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+	       'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+	       'org.codehaus.groovy.grails.web.mapping', // URL mapping
+	       'org.codehaus.groovy.grails.commons', // core / classloading
+	       'org.codehaus.groovy.grails.plugins', // plugins
+	       'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+	       'org.springframework',
+	       'org.hibernate'
 
+    warn   'org.mortbay.log'
 
-	off 'org.springframework',
-			'org.hibernate'
+	debug	'grails.app'
+}
 
-	debug 'grails.app'
-
-	warn 'org.mortbay.log'
 }
 
 
