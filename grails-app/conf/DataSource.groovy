c0ac2a56302107bf64edb7f14f75727d047b6632
abnun <abnun@gmx.de>
2016-01-26 01:37:53 +0100

    - Klammersetzung falsch

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -6,14 +6,14 @@ dataSource {
     password = "autohutt4postgres"
 }
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
 //    cache.provider_class='net.sf.ehcache.hibernate.EhCacheRegionFactory'
-    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
-	development {
+    development {
         dataSource {
 //          dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
@@ -23,63 +23,63 @@ environments {
             logSql = true
             formatSql = true
         }
-        /*dataSource {
-			dbCreate = "update"
-			url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-			pooled = true
-			properties {
-				maxActive = -1
-				minEvictableIdleTimeMillis=1800000
-				timeBetweenEvictionRunsMillis=1800000
-				numTestsPerEvictionRun=3
-				testOnBorrow=true
-				testWhileIdle=true
-				testOnReturn=true
-				validationQuery="SELECT 1"
-			}
-		}*/
-        test {
-            dataSource {
-                dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-
-                url = "jdbc:postgresql://webmpuls.de:5432/autohutt-int"
-
-                logSql = true
-                formatSql = true
-            }
-        }
-
-        /*test {
-            dataSource {
-                dbCreate = "update"
-                url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            }
-        }*/
-
-        production {
-            dataSource {
-                dbCreate = "update"
-
-                url = "jdbc:postgresql://webmpuls.de:5432/autohutt-prod"
-            }
-        }
-
-        /*production {
-            dataSource {
-                dbCreate = "update"
-                url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-                pooled = true
-                properties {
-                    maxActive = -1
-                    minEvictableIdleTimeMillis=1800000
-                    timeBetweenEvictionRunsMillis=1800000
-                    numTestsPerEvictionRun=3
-                    testOnBorrow=true
-                    testWhileIdle=true
-                    testOnReturn=true
-                    validationQuery="SELECT 1"
-                }
-            }
-        }*/
     }
+    /*dataSource {
+        dbCreate = "update"
+        url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+        pooled = true
+        properties {
+            maxActive = -1
+            minEvictableIdleTimeMillis=1800000
+            timeBetweenEvictionRunsMillis=1800000
+            numTestsPerEvictionRun=3
+            testOnBorrow=true
+            testWhileIdle=true
+            testOnReturn=true
+            validationQuery="SELECT 1"
+        }
+    }*/
+    test {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+
+            url = "jdbc:postgresql://webmpuls.de:5432/autohutt-int"
+
+            logSql = true
+            formatSql = true
+        }
+    }
+
+    /*test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+        }
+    }*/
+
+    production {
+        dataSource {
+            dbCreate = "update"
+
+            url = "jdbc:postgresql://webmpuls.de:5432/autohutt-prod"
+        }
+    }
+
+    /*production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            pooled = true
+            properties {
+                maxActive = -1
+                minEvictableIdleTimeMillis=1800000
+                timeBetweenEvictionRunsMillis=1800000
+                numTestsPerEvictionRun=3
+                testOnBorrow=true
+                testWhileIdle=true
+                testOnReturn=true
+                validationQuery="SELECT 1"
+            }
+        }
+    }*/
 }
